bfac7a3e4a5fa07ebd8d8316ce7a43dc1daceb15
Flavian Jumba <flaviajumba@gmail.com>
2024-07-12 11:33:36 +0300

    made the simple question app

--- quiz.js
+++ quiz.js
@@ -1,6 +1,15 @@
-const btn=document.querySelector('#quiz-container #submit-answer' );
-btn.addEventListener('click', function(e){
-    function(checkAnswer){
-        
+function checkAnswer(){
+    //correct answer 
+    let correctAnswer='4';
+    //Retrieving the user's answer
+    const userAnswer=document.querySelector('input[name="quiz"]:checked')?.Value;
+    //Checking if the user's answer is correct
+    const feedbackElement=document.getElementById('feedback');
+    if(correctAnswer==userAnswer){
+        feedbackElement.textContent='Correct! Well done.';
+    }else{
+        feedbackElement.textContent='That is incorrect.Try again!';
     }
-})
+}
+//Adding an event listener to the submit button
+document.getElementById('submit-answer').addEventListener('click',checkAnswer);
