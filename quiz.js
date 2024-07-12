83f39480b6a9768992740cf6e10ad96895ed35ed
Flavian Jumba <flaviajumba@gmail.com>
2024-07-12 10:06:36 +0300

    made changes in the JS file

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 const btn=document.querySelector('#quiz-container #submit-answer' );
 btn.addEventListener('click', function(e){
     function(checkAnswer){
-        let correctAnswer='4'
+        
     }
 })
